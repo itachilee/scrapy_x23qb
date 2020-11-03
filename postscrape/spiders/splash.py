cf2d7a141fd7fd832661cd40c3e0faeef94fcfda
itachilee <3050155685@qq.com>
2020-11-03 16:11:28 +0800

    add except handler

--- postscrape/spiders/splash.py
+++ postscrape/spiders/splash.py
@@ -12,7 +12,11 @@ class SplashSpider(scrapy.Spider):
 
     def parse(self, response):
         jdict = json.loads(response.body)
-        total_pages = int(jdict['total_pages'])
+        try:
+            total_pages = int(jdict['total_pages'])
+        except RuntimeError:
+            total_pages = 0
+            print('获取总页数失败')
         results = jdict['results']
         for each in results:
             image_url = each['urls']['regular']
@@ -21,7 +25,11 @@ class SplashSpider(scrapy.Spider):
             item['title'] = each['alt_description']
             item['image_urls'] = [image_url]
             yield item
-        pn = int(response.url.split('=')[-1])
+        try:
+            pn = int(response.url.split('=')[-1])
+        except RuntimeError:
+            pn = 999999
+            print('获取当前页数失败')
         if pn < total_pages:
             next_pn = int(pn + 1)
             print('共{%d}页，已下载{%d}页' % (total_pages, pn))
@@ -30,5 +38,6 @@ class SplashSpider(scrapy.Spider):
             yield scrapy.Request(
                 next_url,
                 callback=self.parse,
-                dont_filter=True
+                dont_filter=True,
+                meta={'dont_redirect': True, 'handle_httpstatus_list': [302]}
             )
